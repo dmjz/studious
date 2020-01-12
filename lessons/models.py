d9318880e1e4920573c4481fc1229abd0e4740df
David Mason <djmot@users.noreply.github.com>
2020-01-12 11:09:05 -0500

    Add Profile model linked to User

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lesson(models.Model):
+    uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created = models.DateTimeField()
