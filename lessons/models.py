49eb3a4aca943b162f98c08e8fb970992a02961f
David Mason <djmot@users.noreply.github.com>
2020-01-12 11:16:12 -0500

    Model fixes

--- lessons/models.py
+++ lessons/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class Lesson(models.Model):
     uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created = models.DateTimeField()
