fc6429413ce52f274568f8c1fb21c43c5eca8057
David Mason <djmot@users.noreply.github.com>
2020-02-04 08:01:09 -0500

    Add review-related fields to lesson model

--- lessons/models.py
+++ lessons/models.py
@@ -3,10 +3,7 @@ import os
 from django.db import models
 from django.contrib.auth.models import User
 from core.models import Profile
-# from django.core.files.storage import FileSystemStorage
 from studious.storage_backends import LessonStorage
-
-# storage = FileSystemStorage()
 
 def user_directory_path(instance, filename):
     """ Return lesson upload filepath: <media folder>/owner_uuid/lesson_uuid """
@@ -14,12 +11,24 @@ def user_directory_path(instance, filename):
     return os.path.join(str(instance.owner.profile.uuid), str(instance.uuid))
 
 class Lesson(models.Model):
+    
+    # UUID for storage on AWS
     uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    
+    # "Metadata" fields
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.TextField(default='Untitled', max_length=500)
     created = models.DateTimeField()
-    # file = models.FileField(upload_to=user_directory_path, storage=storage, max_length=200)
+
+    # File storage of lesson content
     file = models.FileField(upload_to=user_directory_path, storage=LessonStorage(), max_length=200)
 
+    # Review fields
+    review_stage = models.PositiveSmallIntegerField(default=0)
+    next_review_time = models.DateTimeField(default=None, null=True)
+    review_correct = models.PositiveIntegerField(default=0)
+    review_incorrect = models.PositiveIntegerField(default=0)
+    review_started = models.DateTimeField(default=None, null=True)
+
     def __str__(self):
-        return f'<Lesson { self.id }: { self.owner } { self.created }>'
+        return f'<Lesson { self.id }: { self.owner }, { self.created }>'
